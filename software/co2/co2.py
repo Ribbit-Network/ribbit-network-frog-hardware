5ba867efb994770332e4bbc9303d6b8f8305aa32
Keenan Johnson <keenan.johnson@gmail.com>
2021-08-25 19:25:28 -0700

    #16 Enable ASC

--- software/co2/co2.py
+++ software/co2/co2.py
@@ -60,7 +60,7 @@ dps310 = adafruit_dps310.DPS310(i2c_bus)
 # Enable self calibration mode
 scd.temperature_offset = 0
 scd.altitude = 0
-scd.self_calibration_enabled = False
+scd.self_calibration_enabled = True
 
 
 gps_valid = False
