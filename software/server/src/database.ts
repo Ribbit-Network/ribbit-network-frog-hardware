f89257f161cae4b01fa62fbef7f8e1edfb84f08d
timfosteman <Timothy.Fosteman@integraconnect.com>
2022-08-09 15:44:06 -0400

    platform UUID

--- software/server/src/database.ts
+++ software/server/src/database.ts
@@ -2,9 +2,12 @@ import sqlite3 from "sqlite3";
 import { open, Database } from "sqlite";
 import { SensorReading } from "./types";
 import { makeAutoObservable } from "mobx";
+import { v5, v4 as uuidv4 } from "uuid";
 
 class AppDAO {
   db: Database;
+
+  platformUUID?: string;
 
   constructor() {
     this.init();
@@ -13,7 +16,6 @@ class AppDAO {
   }
 
   async init() {
-    console.log("Initializing db");
     sqlite3.verbose();
 
     this.db = await open({
@@ -28,8 +30,11 @@ class AppDAO {
 
     console.log("[DB] Initializing sensorReadings table...");
     await this.db.run(
-      "CREATE TABLE IF NOT EXISTS sensorReadings(timestamp TEXT, co2 TEXT, temp TEXT, relative_humidity TEXT, lat TEXT, lon TEXT, alt TEXT, scd_temp_offset TEXT, baro_temp TEXT, baro_pressure_hpa TEXT, scd30_pressure_mbar TEXT, scd30_alt_m TEXT)"
+      "CREATE TABLE IF NOT EXISTS sensorReadings(uuid TEXT, timestamp TEXT, co2 TEXT, temp TEXT, relative_humidity TEXT, lat TEXT, lon TEXT, alt TEXT, scd_temp_offset TEXT, baro_temp TEXT, baro_pressure_hpa TEXT, scd30_pressure_mbar TEXT, scd30_alt_m TEXT)"
     );
+
+    const platformUUID = await this.getPlatformUUID();
+    console.log("[DB] Platform UUID recognized:", platformUUID);
   }
 
   async addSensorReading(reading: SensorReading) {
@@ -47,13 +52,39 @@ class AppDAO {
       scd_temp_offset,
     } = reading;
 
+    // generating uuid using a platformUUID namespace
+    const uuid = v5(uuidv4(), await this.getPlatformUUID());
+
     await this.db.exec(
-      `INSERT INTO sensorReadings VALUES (${Date.now()}, ${co2}, ${temp}, ${relative_humidity}, ${lat}, ${lon}, ${alt}, ${scd_temp_offset}, ${baro_temp}, ${baro_pressure_hpa}, ${scd30_pressure_mbar}, ${scd30_alt_m})`,
+      `INSERT INTO sensorReadings VALUES (${uuid}, ${Date.now()}, ${co2}, ${temp}, ${relative_humidity}, ${lat}, ${lon}, ${alt}, ${scd_temp_offset}, ${baro_temp}, ${baro_pressure_hpa}, ${scd30_pressure_mbar}, ${scd30_alt_m})`,
       reading
     );
   }
+
   async getAllSensorReadings(): Promise<SensorReading[]> {
     return this.db.all<SensorReading[]>("SELECT * FROM sensorReadings");
+  }
+
+  async getPlatformUUID(): Promise<string | undefined> {
+    if (this.platformUUID) return this.platformUUID;
+
+    const platformUUID = await this.db.get(
+      "SELECT name, value FROM settings WHERE name == 'platformUUID'"
+    );
+
+    if (platformUUID?.value) {
+      this.platformUUID = platformUUID.value;
+    } else {
+      const newUUID = uuidv4();
+      console.log("[DB] initializing platform UUID", newUUID);
+      await this.db.exec(
+        `INSERT INTO settings VALUES ("platformUUID", "${newUUID}")`
+      );
+
+      this.platformUUID = newUUID;
+    }
+
+    return this.platformUUID;
   }
 }
 
