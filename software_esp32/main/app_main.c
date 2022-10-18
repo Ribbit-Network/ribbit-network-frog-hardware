03a614115960a03459186b30151c11f181ee8aa0
Keenan Johnson <keenan.johnson@gmail.com>
2022-10-18 16:26:26 +0900

    #175 Code now reads SCD30 sensor data

--- software_esp32/main/app_main.c
+++ software_esp32/main/app_main.c
@@ -7,6 +7,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include <esp_err.h>
 #include "nvs.h"
 #include "shell.h"
 #include "wifi.h"
@@ -18,6 +19,10 @@
 #include "scd30.h"
 
 #define TAG "golioth_example"
+
+#ifndef APP_CPU_NUM
+#define APP_CPU_NUM PRO_CPU_NUM
+#endif
 
 // Current firmware version
 static const char* _current_version = "1.2.3";
@@ -115,6 +120,54 @@ static golioth_settings_status_t on_setting(
 
     // If the setting is not recognized, we should return an error
     return GOLIOTH_SETTINGS_KEY_NOT_RECOGNIZED;
+}
+
+
+void scd30_task(void *pvParameters)
+{
+    i2c_dev_t dev = {0};
+
+    ESP_ERROR_CHECK(scd30_init_desc(&dev, 0, CONFIG_SCD30_I2C_MASTER_SDA, CONFIG_SCD30_I2C_MASTER_SCL));
+
+    uint16_t version, major_ver, minor_ver;
+    ESP_ERROR_CHECK(scd30_read_firmware_version(&dev, &version));
+
+    major_ver = (version >> 8) & 0xf;
+    minor_ver = version & 0xf;
+
+    ESP_LOGI(TAG, "SCD30 Firmware Version: %d.%d", major_ver, minor_ver);
+
+    ESP_LOGI(TAG, "Starting continuous measurement");
+    ESP_ERROR_CHECK(scd30_trigger_continuous_measurement(&dev, 0));
+
+    float co2, temperature, humidity;
+    bool data_ready;
+    while (1)
+    {
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        scd30_get_data_ready_status(&dev, &data_ready);
+
+        if (data_ready)
+        {
+            esp_err_t res = scd30_read_measurement(&dev, &co2, &temperature, &humidity);
+            if (res != ESP_OK)
+            {
+                ESP_LOGE(TAG, "Error reading results %d (%s)", res, esp_err_to_name(res));
+                continue;
+            }
+
+            if (co2 == 0)
+            {
+                ESP_LOGW(TAG, "Invalid sample detected, skipping");
+                continue;
+            }
+
+            ESP_LOGI(TAG, "CO2: %.0f ppm", co2);
+            ESP_LOGI(TAG, "Temperature: %.2f °C", temperature);
+            ESP_LOGI(TAG, "Humidity: %.2f %%", humidity);
+        }
+    }
 }
 
 void app_main(void) {
@@ -272,6 +325,11 @@ void app_main(void) {
     // for each setting.
     golioth_settings_register_callback(client, on_setting);
 
+
+    // Set up the SCD30 task
+    ESP_ERROR_CHECK(i2cdev_init());
+    xTaskCreatePinnedToCore(scd30_task, "test", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL, APP_CPU_NUM);
+
     // Now we'll just sit in a loop and update a LightDB state variable every
     // once in a while.
     ESP_LOGI(TAG, "Entering endless loop");
@@ -284,15 +342,4 @@ void app_main(void) {
         counter++;
         vTaskDelay(_loop_delay_s * 1000 / portTICK_PERIOD_MS);
     };
-
-    // That pretty much covers the basics of this SDK!
-    //
-    // If you log into the Golioth console, you should see the log messages and
-    // LightDB state should look something like this:
-    //
-    // {
-    //      "counter": 10,
-    //      "my_int": 42,
-    //      "my_string": "asdf"
-    // }
 }
